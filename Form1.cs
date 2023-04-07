affe0020e19de46610ca6914fb5e66d69fdcdb70
nikitafront <nikitapopov1295@gmail.com>
2023-04-07 20:41:55 +0500

    added image assets, moving logic

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,70 @@
 namespace SpaceGame;
 
-public partial class Form1 : Form
+public sealed partial class Form1 : Form
 {
+    private event Action<object?, KeyEventArgs, PictureBox, int> playerMoving;
+    private readonly string pathToAssets = Path.GetFullPath("..\\..\\..\\assets\\");
+    private readonly Size playerSize = new Size(68, 77);
+
     public Form1()
     {
-        InitializeComponent();
+        var newPlayerLocation = Point.Empty;
+        
+        playerMoving += (sender, args, picture, offsetValue) =>
+        {
+            newPlayerLocation = Point.Empty;
+            switch (args.KeyCode)
+            {
+                case Keys.Up:
+                    newPlayerLocation.Y = picture.Location.Y <= 0 ? 0 : -offsetValue;
+                    break;
+                case Keys.Down:
+                    newPlayerLocation.Y = picture.Location.Y + playerSize.Height * 1.60 >= Size.Height ? 0 : offsetValue;
+                    break;
+                case Keys.Left:
+                    newPlayerLocation.X = picture.Location.X <= 0 ? 0 : -offsetValue;
+                    break;
+                case Keys.Right:
+                    newPlayerLocation.X = picture.Location.X + playerSize.Width * 1.35 >= Size.Width ? 0 : offsetValue;
+                    break;
+            }
+        };
+
+        {
+            Text = "SpaceGame";
+            Size = new Size(1240, 720);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            BackgroundImage = Image.FromFile(pathToAssets + "mainBackgroundImages\\1.jpg"); //TODO make background animated 
+        }
+
+        var player = new PictureBox
+        {
+            Size = playerSize,
+            Location = new Point(ClientSize.Width / 2, ClientSize.Height - 77),
+            BackColor = Color.Transparent,
+            BackgroundImage = Image.FromFile(Path.GetFullPath(pathToAssets + "player.png"))
+        };
+
+        var timer = new System.Windows.Forms.Timer
+        {
+            Enabled = true,
+            Interval = 20
+        };
+        timer.Tick += (sender, args) =>
+        {
+            player.Location = new Point(
+                player.Location.X + newPlayerLocation.X,
+                player.Location.Y + newPlayerLocation.Y
+            );
+            newPlayerLocation = new Point(newPlayerLocation.X * 4 / 10, newPlayerLocation.Y * 4 / 10);
+        };
+
+        KeyDown += (sender, args) =>
+        {
+            playerMoving(sender, args, player, 10);
+        };
+        
+        Controls.Add(player);
     }
 }
